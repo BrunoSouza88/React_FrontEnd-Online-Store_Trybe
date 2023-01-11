760531c3ce0b6f9379030a2e0c4a9d8834eab2ce
BrunoSouza <desouza88@hotmail.com>
2023-01-11 19:50:33 -0300

    Grupo 10 - Requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as api from '../services/api';
 
 class Home extends React.Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.qualquerUm();
+  }
+
+  qualquerUm = async () => {
+    const categories = await api.getCategories();
+    console.log(categories);
+    this.setState({
+      categories,
+    });
+  };
+
   render() {
+    const { categories } = this.state;
+
     return (
       <div>
         <input type="text" name="" id="" />
@@ -17,7 +36,23 @@ class Home extends React.Component {
             Carrinho de Compras
           </button>
         </Link>
-
+        <div>
+          <h2>
+            Categorias:
+          </h2>
+          <div>
+            { categories
+              .map((category) => (
+                <button
+                  type="button"
+                  data-testid="category"
+                  key={ category.id }
+                >
+                  {category.name}
+                </button>
+              ))}
+          </div>
+        </div>
       </div>
     );
   }

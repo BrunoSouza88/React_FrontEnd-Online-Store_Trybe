cb8b318e709aa0c8dc9de9709c1bd53dfc169fce
Hector Munhoz <hectormunhozc@gmail.com>
2023-01-12 19:02:24 -0300

    Requisito 07

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route exact path="/cart" component={ Cart } />
+        <Route exact path="/product" component={ Product } />
       </Switch>
     </div>
   );
